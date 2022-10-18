478b264e914da7d5d571671aae93e41ac755deee
FraSma <francemazzi@gmail.com>
2022-10-18 20:11:45 +0200

    modifica cart

--- components/cartItem.ts
+++ components/cartItem.ts
@@ -1,6 +1,7 @@
 const titleDisplay = document.querySelector("#title");
 
 export function populateUser(isReturning: boolean, nomePrimo: string) {
+  console.log(titleDisplay);
   const accesso = isReturning ? "Bentornato" : "";
   titleDisplay.innerHTML = `<h2>${accesso} ${nomePrimo}, il tuo carrello contiene: </h2>`;
 }
