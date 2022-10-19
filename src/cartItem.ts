8d4fb596ab01bdaf3b0ce8e670a5e16c899db087
FraSma <francemazzi@gmail.com>
2022-10-19 13:25:35 +0200

    ciclo img non funziona

--- src/cartItem.ts
+++ src/cartItem.ts
@@ -1,8 +1,58 @@
+import { products } from './index';
 const titleDisplay = document.querySelector('#title') as HTMLDivElement;
+const tabellaDisplay = document.querySelector(
+  '#tabellaCarta',
+) as HTMLDivElement;
 
+//Welcome
 export function populateUser(isReturning: boolean, nomePrimo: string) {
-  console.log(isReturning, nomePrimo);
-
   const accesso = isReturning ? 'Bentornato' : '';
   titleDisplay.innerHTML = `<h2>${accesso} ${nomePrimo}, il tuo carrello contiene: </h2>`;
 }
+
+// product item
+export const creaProdotto = (
+  prodotti: number,
+  immagine: string,
+  titoloProdotto: string,
+  costoDiConsegna: number,
+  prezzo: number,
+  spedizioneData: string,
+) => {
+  for (let i = 0; i < products.length; i++) {
+    const prodottoItem = document.createElement('div');
+    tabellaDisplay.appendChild(prodottoItem);
+    prodottoItem.classList.add('prodottoItem');
+    const image = document.createElement('img');
+    image.setAttribute('src', products[i].immagine);
+    prodottoItem.innerHTML = `
+          <div id="img">
+            <img src="${products[i].immagine}" alt="" />
+          </div>
+          <div>
+            <span>Prodotto:</span>
+            <div id="descrizioneProdottoItem">
+              ${products[i].titoloProdotto}
+            </div>
+          </div>
+          <div>
+            <span>Costo di consegna:</span>
+            <div id="prezzoSpedizione">${products[i].costoDiConsegna}</div>
+          </div>
+          <div>
+            <span>Spedizione</span>
+            <div id="dataSpedizione">${products[i].spedizioneData}</div>
+          </div>
+          <div>
+            <button id="addButton" class="btn">+</button
+            ><input type="text" /><button class="btn" id="removeButton">
+              -
+            </button>
+          </div>
+          <div>
+            <span>Totale</span>
+            <div id="prezzoTotale">${products[i].prezzo}</div>
+          </div>
+    `;
+  }
+};
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
-import { populateUser } from './cartItem';
+// import { v4 as uuidV4 } from 'uuid';
+// uuidV4();
+import { populateUser, creaProdotto } from './cartItem';
 
 //dati utente
 const you: {
@@ -14,7 +16,7 @@ const you: {
 ];
 
 //Dati carrello
-const products: {
+export const products: {
   immagine: string;
   titoloProdotto: string;
   costoDiConsegna: number;
@@ -22,13 +24,43 @@ const products: {
   spedizioneData: string;
 }[] = [
   {
-    immagine:
-      'img/CoffeBreak_Roscioli_salato_Cosaporto-e1593435853690-300x300.png',
+    immagine: '/public/img/iTem1.png',
     titoloProdotto:
       'TRAMEZZINI Prosciutto di Praga e mozzarella di Bufala + maionese (x1), Prosciutto crudo e mozzarella di Bufala + maionese (x1) MARITOZZI SALATI Baccalà mantecato e lamponi (x1), Mortadella, ricotta e granella di pistacchi (x1)',
     costoDiConsegna: 6.9,
     prezzo: 39,
-    spedizioneData: 'Dalle 08:30 del 19-10-2022',
+    spedizioneData: 'Dalle 08:30 del 21-10-2022',
+  },
+  {
+    immagine: '/public/img/iTem.2.jpg',
+    titoloProdotto: 'Cake al Limone da 6 a 12 porzioni 6/8 porzioni',
+    costoDiConsegna: 6.9,
+    prezzo: 22,
+    spedizioneData: 'Dalle 08:30 del 21-10-2022',
+  },
+  {
+    immagine: '/public/img/item3.jpg',
+    titoloProdotto:
+      'RINVENIMENTO La Vaporiera (x1) SFIZI Edamame - classico/piccante - Classico (x1), Edamame - classico/piccante - Piccante (x1), Pickles - Cetriolini Sottaceto (x1)',
+    costoDiConsegna: 6.9,
+    prezzo: 19,
+    spedizioneData: 'Dalle 18:00 di oggi',
+  },
+  {
+    immagine: '/public/img/item4.jpg',
+    titoloProdotto: 'Secchiello in zinco delicato  ',
+    costoDiConsegna: 6.9,
+    prezzo: 50,
+    spedizioneData: 'Dalle 15:00 di oggi',
   },
 ];
+let i = 0;
 populateUser(you[0].isReturning, you[0].nome);
+creaProdotto(
+  products.length,
+  products[i].immagine,
+  products[i].titoloProdotto,
+  products[i].costoDiConsegna,
+  products[i].prezzo,
+  products[i].spedizioneData,
+);
