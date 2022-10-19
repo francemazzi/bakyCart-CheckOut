cd539aa22c164659f58a5b6713a2a19250fbc9a5
FraSma <francemazzi@gmail.com>
2022-10-19 12:03:15 +0200

    aggiunta snowpack

--- src/cartItem.ts
+++ src/cartItem.ts
@@ -1,8 +1,8 @@
-const titleDisplay = document.querySelector("#title") as HTMLDivElement;
+const titleDisplay = document.querySelector('#title') as HTMLDivElement;
 
 export function populateUser(isReturning: boolean, nomePrimo: string) {
   console.log(isReturning, nomePrimo);
 
-  const accesso = isReturning ? "Bentornato" : "";
+  const accesso = isReturning ? 'Bentornato' : '';
   titleDisplay.innerHTML = `<h2>${accesso} ${nomePrimo}, il tuo carrello contiene: </h2>`;
 }
